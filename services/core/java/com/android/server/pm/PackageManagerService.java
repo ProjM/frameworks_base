b2b0a900931a8eaaba4b60c91d717ae5c200b440
fusionjack <dogfight60-fusionjack@yahoo.de>
2016-06-26 02:48:30 +0200

    PackageManager: Avoid possible bootloop

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -2459,10 +2459,6 @@ public class PackageManagerService extends IPackageManager.Stub {
                 continue;
             }
 
-            if (requiredInstaller != null) {
-                throw new RuntimeException("There must be one required installer");
-            }
-
             requiredInstaller = packageName;
         }
 
